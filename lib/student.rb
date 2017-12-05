34fce9e580865db95838cf026a749a7780919d06
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-05 02:57:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,10 @@ class Student
   end
 
   def self.new_from_db(row)
-
+    new_student = Self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
   end
 
   def self.find_by_name(name)
