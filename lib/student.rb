1f31a408243cfa82e9a7011f76e82f9b92ce9d84
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-05 02:53:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
     SQL
 
     result = DB[:conn].execute(sql, name)[0]
-    Song.new(result[0], result[1], result[2])
+    Student.new(result[0], result[1], result[2])
   end
 
   def update
